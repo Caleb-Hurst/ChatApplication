be0faf644599cb65392f260f0a83dcb01b6ff72b
Caleb-Hurst <hursty.caleb@gmail.com>
2023-04-30 12:29:00 -0500

    First full function push
    everything seems to be working

--- src/main/java/com/coderscampus/ChatApp/web/ChannelController.java
+++ src/main/java/com/coderscampus/ChatApp/web/ChannelController.java
@@ -3,17 +3,20 @@ package com.coderscampus.ChatApp.web;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.coderscampus.ChatApp.domain.Channel;
 import com.coderscampus.ChatApp.domain.Message;
+import com.coderscampus.ChatApp.domain.User;
 import com.coderscampus.ChatApp.service.ChannelService;
-import com.coderscampus.ChatApp.service.MessageService;
 import com.coderscampus.ChatApp.service.UserService;
 
 @Controller
@@ -45,7 +48,10 @@ public class ChannelController {
 	public String getChannelData(@PathVariable Long channelId, Model model) {
 	    Channel channel = channelService.findById(channelId);
 	    model.addAttribute("channel", channel);
+	    model.addAttribute("channelId", channel.getChannelId());
+	    model.addAttribute("newMessage", new Message());
 	    // add code to retrieve messages by channelId and add to model
 	    return "channel";
 	}
+	
 }
--- src/main/java/com/coderscampus/ChatApp/web/MessageController.java
+++ src/main/java/com/coderscampus/ChatApp/web/MessageController.java
@@ -3,14 +3,17 @@ package com.coderscampus.ChatApp.web;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.coderscampus.ChatApp.domain.Channel;
+import com.coderscampus.ChatApp.domain.Message;
 import com.coderscampus.ChatApp.domain.User;
 import com.coderscampus.ChatApp.service.ChannelService;
 import com.coderscampus.ChatApp.service.UserService;
@@ -23,6 +26,26 @@ public class MessageController {
 	@Autowired 
 	private ChannelService channelService;
 	
+	@PostMapping("/channel/{channelId}")
+	public ResponseEntity<String> sendMessage(@PathVariable Long channelId, @RequestParam("message") String message,@RequestParam("user") String user) {	   
+	    Channel channel = channelService.findById(channelId);
+	    User newUser = userService.findByName(user);
+	    Message newMessage = new Message();
+	    newMessage.setMessage(message);
+	    newMessage.setUser(newUser);
+	    newMessage.setChannel(channel);
+	    channel.getMessages().add(newMessage);
+	    channelService.save(channel);
+	    return ResponseEntity
+	            .status(HttpStatus.OK)
+	            .body("{\"status\": \"success\"}");
+	}
+	@GetMapping("/channel/{channelId}/messages")
+	@ResponseBody
+	public List<Message> getChannelMessages(@PathVariable Long channelId) {
+	   Channel channel = channelService.findById(channelId);
+	   return channel.getMessages();
+	}
 
 
 }
